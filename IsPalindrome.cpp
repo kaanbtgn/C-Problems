61072c0523be3e331a824d5ab4c7c9976c3c1185
Kaan Bitgin <79223693+kaanbtgn@users.noreply.github.com>
2022-12-15 16:01:20 +0300

    Add files via upload

---
+++ IsPalindrome.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+bool isPalindrome(int x);
+int main()
+{    vector <int> v;
+	v.push_back(121);
+	v.push_back(-121);
+	v.push_back(10);
+    v.push_back(121);
+	v.push_back(12321);
+	for (int i = 0; i < v.size(); i++)
+	{
+        int val = isPalindrome(v[i]);
+        if (val==1)
+			cout << v[i] <<" Polindrome Number" << endl;
+        else
+			cout << v[i] <<" Not a Polindrome Number" << endl;        
+	} 
+}
+bool isPalindrome(int x) {
+    if (x < 0 || (x % 10 == 0 && x != 0)) {
+        return false;
+    }
+    int revertedNumber = 0;
+    while (x > revertedNumber) {
+        revertedNumber = revertedNumber * 10 + x % 10;
+        x /= 10;
+    }
+	return x == revertedNumber || x == revertedNumber / 10;
+}
